f408428fc1ac067541938e81430120c3c57e5cc4
Landon Swartz <lgsm2n@mail.missouri.edu>
2019-06-13 14:07:27 -0500

    Added basic plate and well classes to SPIPware entities namespace

---
+++ SPIPware/Entities/Plate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPIPware.Entities
+{
+    //for collection of wells
+    class Plate
+    {
+        public int numWells; //how many wells in the plate
+    }
+}
---
+++ SPIPware/Entities/Well.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPIPware.Entities
+{
+    class Well
+    {
+        public int radius; //size of well
+        public int diameter; //diameter of well
+
+        public int GetDiameter(radius)
+        {
+            int diameter = radius * 2;
+
+            return diameter;
+        }
+
+
+        public int WellArea(radius)
+        {
+            double area = (radius * radius) * 3.14;
+
+            return area;
+        }
+    }
+}
