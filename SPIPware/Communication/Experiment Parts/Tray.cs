b8a0ead891b110036c0265ca549402750e8e67e6
Landon Swartz <lgsm2n@mail.missouri.edu>
2019-06-17 13:38:26 -0500

    Comment about what parts were legacy code in GCodeFile. Improved TODO list. Remodel Flowchart. Made experiment part classes into own folder in communication namespace.

---
+++ SPIPware/Communication/Experiment Parts/Tray.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPIPware.Communication
+{
+    class Tray
+    {
+        Plate[,] plates = new Plate[3,4]; //need array size but maybe hard coded
+    }
+}
